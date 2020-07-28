aaa25b2ac6d4b93e7f4d0da521832458257094c7
mikouhero <290468335@qq.com>
2020-07-28 23:32:41 +0800

    升级版冒泡

--- Algorithm/1、冒泡排序/BubbleSort.go
+++ Algorithm/1、冒泡排序/BubbleSort.go
@@ -5,6 +5,8 @@ import "fmt"
 func main()  {
 	arr:=[]int{3,4,5,1,2,7,6,9,8}
 	BubbleSort1(arr)
+	BubbleSort2(arr)
+
 }
 
 
@@ -15,6 +17,18 @@ func BubbleSort1( arr []int){
 		for j:=0;j<len ;j++  {
 			if arr[i] < arr[j] {
 				arr[i],arr[j] = arr[j],arr[i]
+			}
+		}
+	}
+	fmt.Println(arr)
+}
+
+func BubbleSort2(arr []int)  {
+	len := len(arr)
+	for i:=1;i<len ;i++  {
+		for j:=0;j<len-i ;j++  {
+			if arr[j] < arr[j+1] {
+				arr[j],arr[j+1] = arr[j],arr[j+1]
 			}
 		}
 	}
--- Algorithm/1、冒泡排序/BubbleSort2.php
+++ Algorithm/1、冒泡排序/BubbleSort2.php
@@ -1,19 +1,19 @@
     <?php
 
-    function BubbleSort2($array)
+    function BubbleSort2($arr)
     {
-        $len = count($array);
+        $len = count($arr);
 
-        for ( $i=1; $i<$len; $i++ ) {
-              for($j=$len-1;$j>=$i; $j-- ){
-                  if( $array[$j] > $array[$j+1]) {
-                      $temp = $array[$i];
-                      $array[$i] = $array[$j];
-                      $array[$j] = $temp;
-                  }
-              }
-        }
-        return $array;
+         for ($i = 1; $i < $len; $i++) { //该层循环用来控制每轮 冒出一个数 需要比较的次数
+                for ($k = 0; $k < $len - $i; $k++) {
+                    if ($arr[$k] > $arr[$k + 1]) {
+                        $tmp = $arr[$k + 1];
+                        $arr[$k + 1] = $arr[$k];
+                        $arr[$k] = $tmp;
+                    }
+                }
+            }
+        return $arr;
     }
 
     $array = [9,1,5,8,3,7,6,4,2];
