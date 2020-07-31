898a97ef6861b9f0c8fbc9cc63d9aa65c99b366c
mikouhero <33013997+mikouhero@users.noreply.github.com>
2020-07-31 14:11:02 +0800

    Update quick.go

--- Algorithm/05、快速排序/quick.go
+++ Algorithm/05、快速排序/quick.go
@@ -29,3 +29,40 @@ func quick(arr []int) []int {
 	rightArray = quick(rightArray)
 	return append(append(leftArray, base), rightArray...)
 }
+
+
+func quick2(arr []int, left, right int) {
+	if left >= right {
+		return
+	}
+	// 设置一个参照值
+	temp := arr[left]
+	// 定义两个游标
+	i, j := left, right
+	for {
+		// 从右边开始 找到第一个比参照值小的数
+		for arr[j] >= temp && j > i {
+			// 没找到 继续往左边找 ，当i j  想等 退出
+			j--
+		}
+		// 从左向右找，找到第一个比参照值大的数
+		for arr[i] <= temp && j > i {
+			// 没找到 继续往右边找 ，当i j  想等 退出
+			i++
+		}
+		// 如果 i >=j   说明没找到 ，退出，要么temp 最大 要么最小
+		if i >= j {
+			break
+		}
+		// 如果在中间有遇到 交换数据
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+	// 将 参照值移到游标相遇点
+	arr[left] = arr[i]
+	arr[i] = temp
+	// 递归，左右两侧分别排序
+	quickSort(arr, left, i-1)
+	quickSort(arr, i+1, right)
+}
+
