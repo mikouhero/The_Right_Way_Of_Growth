12c49920852e939171ed74c9b53470441ae539c8
mikouhero <33013997+mikouhero@users.noreply.github.com>
2020-08-03 18:30:46 +0800

    Create shell.php

---
+++ Algorithm/08、希尔排序/shell.php
@@ -0,0 +1,25 @@
+$a =[0, 9, 1, 5, 8, 3, 7, 6, 4, 2];
+shell($a);
+
+function shell($arr)
+{
+    $len = count($arr);
+    $gap = floor($len / 2);
+    while ($gap > 0) {
+
+        for ($i = $gap; $i < $len; $i++) {
+
+            for ($j = $i; $j - $gap >= 0; $j -= $gap) {
+
+                if ($arr[$j] < $arr[$j - $gap]) {
+                    $temp    = $arr[$j];
+                    $arr[$j] = $arr[$j - $gap];
+                    $arr[$j - $gap] = $temp;
+                }
+            }
+        }
+
+        $gap = floor($gap / 2);
+    }
+    print_r($arr);
+}
