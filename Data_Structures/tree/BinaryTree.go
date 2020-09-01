d21de6c8ea8e6b2d366982c89fd17c1d06168057
mikouhero <290468335@qq.com>
2020-09-01 21:11:13 +0800

    树的前中后遍历

--- Data_Structures/tree/BinaryTree.go
+++ Data_Structures/tree/BinaryTree.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Node struct {
 	Data  int
 	Left  *Node
@@ -68,7 +72,6 @@ func (bst *BinaryTree) isin(n *Node, data int) bool {
 	}
 }
 
-
 func (bst *BinaryTree) FindMax() int {
 	if bst.Size == 0 {
 		panic("空树")
@@ -98,3 +101,73 @@ func (bst *BinaryTree) findmix(n *Node) *Node {
 	}
 }
 
+func (bst *BinaryTree) PreOrder() {
+	bst.preorder(bst.Root)
+}
+
+func (bst *BinaryTree) preorder(node *Node) {
+	if node == nil {
+		return
+	}
+
+	fmt.Println(node.Data)
+	bst.preorder(node.Left)
+	bst.preorder(node.Right)
+}
+
+func (bst *BinaryTree) InOrder() {
+	bst.inorder(bst.Root)
+}
+
+func (bst *BinaryTree) inorder(node *Node) {
+	if node == nil {
+		return
+	}
+
+	bst.inorder(node.Left)
+	fmt.Println(node.Data)
+	bst.inorder(node.Right)
+}
+
+func (bst *BinaryTree) PostOrder() {
+	bst.postorder(bst.Root)
+}
+
+func (bst *BinaryTree) postorder(node *Node) {
+	if node == nil {
+		return
+	}
+
+	bst.postorder(node.Left)
+	bst.postorder(node.Right)
+	fmt.Println(node.Data)
+
+}
+
+func main() {
+	bst := NewBinaryTree()
+	bst.Size=7
+	node1 := &Node{4, nil, nil}
+	node2 := &Node{2, nil, nil}
+	node3 := &Node{6, nil, nil}
+	node4 := &Node{1, nil, nil}
+	node5 := &Node{3, nil, nil}
+	node6 := &Node{5, nil, nil}
+	node7 := &Node{7, nil, nil}
+	bst.Root = node1
+	node1.Left = node2
+	node1.Right = node3
+	node2.Left = node4
+	node2.Right = node5
+	node3.Left = node6
+	node3.Right = node7
+	/*
+			4
+		2 	  6
+	1    3  5   7
+	 */
+
+	//bst.PreOrder()	// 4 2 1 3 6 5 7
+	//bst.PostOrder()  // 1 3 2 5 7 6 4
+	//bst.InOrder()  // 1 2 3 4 5 6 7
+}
